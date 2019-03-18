a1623cd7f672d4b828eb6e17b07057339d38c6de
Ian Hume <Humeian@me.com>
2019-03-18 17:55:48 -0400

    Added basic main menu + scoring

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LevelManager {
+	public static string[] levels = new string[] {"TestScene","Level1","Level2"};
+	public static float[] scores = new float[levels.Length];
+	public static int currentLevel = 0;
+
+	public static void loadLevel(int levelNum) {
+		SceneManager.LoadScene (levels [levelNum]);
+		currentLevel = levelNum;
+	}
+
+	public static void resetLevel() {
+		SceneManager.LoadScene (levels [currentLevel]);
+	}
+
+	public static bool recordScore (float timeScore) {
+		Debug.Log (timeScore);
+		if (scores[currentLevel] == 0.0f || timeScore < scores[currentLevel]) {
+			scores[currentLevel] = timeScore;
+			return true;
+		}
+		return false;
+	}
+}
