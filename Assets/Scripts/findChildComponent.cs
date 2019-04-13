c0c391067627aab52dff25c3c2c272d2436577d2
Ian Hume <Humeian@me.com>
2019-04-13 18:13:34 -0400

    Added In-game UI Canvas Prefab

---
+++ Assets/Scripts/findChildComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findChildComponent : MonoBehaviour {
+
+	public GameObject FindObject(string name)
+	{
+		Debug.Log ("I'm being called");
+		Transform[] trs= gameObject.GetComponentsInChildren<Transform>(true);
+		foreach(Transform t in trs){
+			if(t.name == name){
+				Debug.Log (t.name);
+				return t.gameObject;
+			}
+		}
+		return null;
+	}
+}
