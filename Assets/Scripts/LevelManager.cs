c7fe50fb144930ea83806f94c7dead9b984e8091
Elliot Hume <elliot@hume.ca>
2019-04-13 18:27:54 -0400

    Merge fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public static class LevelManager {
 	public static string[] levels = new string[] {"TestScene","Level1","Level2"};
 	public static float[] scores = new float[levels.Length];
-	public static int currentLevel = 1;
 	public static bool isPaused = false;
 
 	public static void togglePause () {
@@ -19,27 +18,26 @@ public static class LevelManager {
 	}
 	public static void loadLevel(int levelNum) {
 		SceneManager.LoadScene (levelNum);
+		Time.timeScale = 1;
 		isPaused = false;
 	}
 
 	public static void nextLevel() {
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		Time.timeScale = 1;
 		isPaused = false;
 	}
 
 	public static void resetLevel() {
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		Time.timeScale = 1;
 		isPaused = false;
-	}
-
-	public static void pauseGame() {
-		currentLevel = SceneManager.GetActiveScene().buildIndex;
 	}
 
 	public static bool recordScore (float timeScore) {
 		Debug.Log (timeScore);
-		if (scores[currentLevel] == 0.0f || timeScore < scores[currentLevel]) {
-			scores[currentLevel] = timeScore;
+		if (scores[SceneManager.GetActiveScene().buildIndex] == 0.0f || timeScore < scores[SceneManager.GetActiveScene().buildIndex]) {
+			scores[SceneManager.GetActiveScene().buildIndex] = timeScore;
 			return true;
 		}
 		return false;
